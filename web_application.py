00494d74f419b2b21e834a482427e3b498474f0e
han chen <hanchen@handeMacBook-Pro.local>
2016-10-08 16:00:13 -0700

    hehe

--- web_application.py
+++ web_application.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 # controllers
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+#@app.route('/favicon.ico')
+#def favicon():
+#    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 @app.errorhandler(404)
 def page_not_found(e):

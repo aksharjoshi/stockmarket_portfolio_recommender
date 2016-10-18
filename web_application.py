5fe7b22a9a343e8b175eaeb94a1e165e905a7db0
han chen <hanchen@handeMacBook-Pro.local>
2016-10-17 21:41:39 -0700

    hello

--- web_application.py
+++ web_application.py
@@ -46,13 +46,6 @@ def index():
         initialSharePrice = request.form['initialSharePrice']
         buyCommision = request.form['buyCommission']
         taxRate = request.form['taxRate']
-        symbol_name = ""
-        url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
-        result = request.get(url).json()
-        for x in result['ResultSet']['Result']:
-            if x['symbol'] == argv:
-                symbol_name =  x['name']
-                break
         totalSell = allotment * finalSharePrice
         initialPaid = allotment * initialSharePrice
         pureProfit = totalSell - initialPaid
@@ -72,7 +65,7 @@ def index():
         netProfit = '{:.2%}'.format(totalSell - totalCost)
         returnRate = '{:.2%}'.format(returnRate)
         breakEven = '{:.2%}'.format(breakEven)
-        return render_template('calculator.html')
+        return render_template('calculator.html', totalSell=totalSell)
     return render_template('index.html')
 
 @app.route("/logout")

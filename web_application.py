5c988c751f7b3ca58603cc9dbc1efebbc590b121
han chen <hanchen@handeMacBook-Pro.local>
2016-10-17 23:16:17 -0700

    hh

--- web_application.py
+++ web_application.py
@@ -109,5 +109,4 @@ def login():
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 if __name__ == '__main__':
-    app.run()
     app.run(debug = True)

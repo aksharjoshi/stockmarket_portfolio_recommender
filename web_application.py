0f59255f1e67f12a6de5a86690b1ffdf709a02a4
han chen <hanchen@handeMacBook-Pro.local>
2016-11-17 15:13:11 -0800

    aa

--- web_application.py
+++ web_application.py
@@ -187,8 +187,8 @@ def finance_analysis():
                 if x['symbol'] == val:
                     real_name=x['name']
                     break
-            #condition = change + ' ' + '(' + str(perchange) + ')'
-            temp = Stock(real_name, 'hello', price)
+            condition = change + ' ' + '(' + str(perchange) + ')'
+            temp = Stock(real_name, condition, price)
             nameAndValue.append(temp)
         stockname = request.form['stockname']
         total_money = request.form['amount']
